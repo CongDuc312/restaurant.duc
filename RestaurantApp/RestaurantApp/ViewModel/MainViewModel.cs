db2acd9a319b590f2bc7317b677469f85c181ef8
CongDuc312 <trancongduc312201@gmail.com>
2023-03-28 14:45:35 +0700

    first commit

---
+++ RestaurantApp/RestaurantApp/ViewModel/MainViewModel.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace RestaurantApp.ViewModel
+{
+    public class MainViewModel : BaseViewModel
+    {
+        public MainViewModel()
+        {
+            Picks = GetPicks();
+        }
+
+        public List<Pick> Picks { get; set; }
+
+        public ICommand OrderCommand => new Command(() => Application.Current.MainPage.Navigation.PushAsync(new OrderPage()));
+
+        private List<Pick> GetPicks()
+        {
+            return new List<Pick>
+            {
+                new Pick { Title = "Bữa Sáng", Image = "IMG04.png",
+                    Description = "Đặt thực đơn bữa sáng lành mạnh và ấm áp của chúng tôi cho một buổi sáng tuyệt vời" },
+                new Pick { Title = "Bữa Trưa", Image = "IMG05.png",
+                    Description = "Bữa trưa ngon miệng để giữ cho ngày của bạn ngọt ngào và vui vẻ" }
+            };
+        }
+    }
+
+    public class Pick
+    {
+        public string Title { get; set; }
+        public string Image { get; set; }
+        public string Description { get; set; }
+        public string Price { get; set; }
+    }
+
+
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged([CallerMemberName] string name = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+    }
+
+}
---
+++ RestaurantApp/RestaurantApp/ViewModel/OrderViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace RestaurantApp.ViewModel
+{
+    public class OrderViewModel : BaseViewModel
+    {
+        public OrderViewModel()
+        {
+            MenuList = GetMenu();
+        }
+
+        public List<Pick> MenuList { get; set; }
+
+        public ICommand BackCommand => new Command(() => Application.Current.MainPage.Navigation.PopAsync());
+
+        private List<Pick> GetMenu()
+        {
+            return new List<Pick>
+            {
+                new Pick { Title = "Sushi", Image = "IMG05.png", Description = "Shashimi cá hồi và Shusi cá hồi tổng hợp ngon khó cưỡng dành cho 'fan cứng' của Shusi", Price = "299.000đ" },
+                new Pick { Title = "Salad ", Image = "IMG04.png", Description = "Chế biến từ các loại rau củ được thu hoạch tại vườn, đảm bảo vừa ăn ngon vừa tốt cho sức khỏe.", Price = "199.000đ" },
+                new Pick { Title = "Bò ' Lúc Lắc ' ", Image = "IMG01.png", Description = "Burger bò hảo hạng phủ phô mai jack tiêu và thịt xông khói giòn.", Price = "399.000đ" }
+            };
+        }
+    }
+}
